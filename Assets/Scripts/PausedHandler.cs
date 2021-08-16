287dca8b1c6531248d0de3cf84c72d711d4590bd
Mrrebrik <itibsoft@gmail.com>
2021-08-16 19:19:23 +0700

    SettingsPanel

--- Assets/Scripts/PausedHandler.cs
+++ Assets/Scripts/PausedHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PausedHandler : MonoBehaviour
 {
@@ -10,10 +11,15 @@ public class PausedHandler : MonoBehaviour
 	[SerializeField] private GameObject _winPanel;
 	[SerializeField] private Text _textCurrentCompletLevel;
 	public Text _textCountMoleculesReward;
+	private const string _sceneMenu = "_Menu";
 
 	private void Awake()
 	{
 		Instance = this;
+	}
+	public void ContinueToMenu()
+	{
+		SceneManager.LoadScene(_sceneMenu);
 	}
 	public void OpenMapLevels()
 	{
